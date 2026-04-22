7e99c76b4e60d6a7b7d5f4f4e0c5a1a9d979a62d
Akshath G <162152807+Akshath019@users.noreply.github.com>
2026-04-22 21:58:33 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,6 +2,6 @@ import java.util.*;
 
 class Main{
     public static void main(String[] args) {
-        System.out.println("I'd win anyday btw i m the best");
+        System.out.println("hey");
     }
 }
